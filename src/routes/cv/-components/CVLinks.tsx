021278feef800b0bf49f3f1add05b819f618d9e9
Brandon Seng Han Chan <bshchan.theone@gmail.com>
2025-07-10 13:12:43 +1200

    feat: install react-icons and add icons to CV links (remove hover background from badge default variant)

--- src/routes/cv/-components/CVLinks.tsx
+++ src/routes/cv/-components/CVLinks.tsx
@@ -1,17 +1,19 @@
-import { Badge } from "../../../components/ui/badge";
-import { Separator } from "../../../components/ui/separator";
+import { FaGithub, FaLinkedin, FaYoutube } from "react-icons/fa";
+import { Badge } from "@/components/ui/badge";
+import { Separator } from "@/components/ui/separator";
 
 export default function CVLinks() {
 	return (
 		<div className="flex h-5 items-center justify-center gap-4 text-sm">
 			{/* TODO: extract these into a json? maybe not worth the effort */}
-			<Badge asChild>
+			<Badge asChild className="bg-[#0077b5]">
 				<a
 					className="hover:underline"
 					href="https://www.linkedin.com/in/brandon-sh-chan/"
 					rel="noopener"
 					target="_blank"
 				>
+					<FaLinkedin />
 					brandon-sh-chan
 				</a>
 			</Badge>
@@ -23,17 +25,19 @@ export default function CVLinks() {
 					rel="noopener"
 					target="_blank"
 				>
+					<FaGithub />
 					br-Chan
 				</a>
 			</Badge>
 			<Separator orientation="vertical" />
-			<Badge asChild>
+			<Badge asChild className="bg-[#FF0000]">
 				<a
 					className="hover:underline"
 					href="https://www.youtube.com/@br-Chan-g5l"
 					rel="noopener"
 					target="_blank"
 				>
+					<FaYoutube />
 					br-Chan
 				</a>
 			</Badge>
